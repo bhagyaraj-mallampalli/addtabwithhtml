31e76970b492cfede9bdec7440cb2526a2ad69d4
Bhagyaraj Mallampalli <bhagyaraj.mallampalli@vodafone.com>
2019-12-24 10:17:48 +0000

    renaming the folder name

--- src/enhancer/tab.ts
+++ src/enhancer/tab.ts
@@ -4,11 +4,11 @@ import TFS_Build_Contracts = require("TFS/Build/Contracts");
 import TFS_Build_Extension_Contracts = require("TFS/Build/ExtensionContracts");
 import DT_Client = require("TFS/DistributedTask/TaskRestClient");
 
-export class InfoTab extends Controls.BaseControl {	
+export class InfoTab extends Controls.BaseControl {
 	constructor() {
 		super();
 	}
-		
+
 	public initialize(): void {
 		super.initialize();
 		// Get configuration that's shared between extension and the extension host
@@ -17,31 +17,31 @@ export class InfoTab extends Controls.BaseControl {
 		if(sharedConfig) {
 			// register your extension with host through callback
 			sharedConfig.onBuildChanged((build: TFS_Build_Contracts.Build) => {
-				this._initBuildInfo(build);	
-				
+				this._initBuildInfo(build);
+
 				/*
 				* If any task uploaded some data using ##vso[task.addattachment] (https://github.com/Microsoft/vso-agent-tasks/blob/master/docs/authoring/commands.md)
 				* Then you could consume the data using taskclient
 				* sample code -
 				*/
-					// var taskClient = DT_Client.getClient();
-					// taskClient.getPlanAttachments(vsoContext.project.id, "build", build.orchestrationPlan.planId, "ATTACHMENT_TYPE_HERE").then((taskAttachments)=> {
-					// 	$.each(taskAttachments, (index, taskAttachment) => {
-					// 		if (taskAttachment._links && taskAttachment._links.self && taskAttachment._links.self.href) {
-					// 			var link = taskAttachment._links.self.href;
-					// 			var attachmentName = taskAttachment.name;
+					 var taskClient = DT_Client.getClient();
+					 taskClient.getPlanAttachments(vsoContext.project.id, "build", build.orchestrationPlan.planId, "ATTACHMENT_TYPE_HERE").then((taskAttachments)=> {
+					 $.each(taskAttachments, (index, taskAttachment) => {
+					 		if (taskAttachment._links && taskAttachment._links.self && taskAttachment._links.self.href) {
+					 			var link = taskAttachment._links.self.href;
+					 			var attachmentName = taskAttachment.name;
 					// 			// do some thing here
 					//			// see how to get auth https://www.visualstudio.com/en-us/docs/report/analytics/building-extension-against-analytics-service
-					// 		}
-					// 	});
-					// });
-				
+					 		}
+					 	});
+					 });
+
 			});
-		}		
+		}
 	}
-	
+
 	private _initBuildInfo(build: TFS_Build_Contracts.Build) {
-		
+
 	}
 }
 
@@ -49,5 +49,3 @@ InfoTab.enhance(InfoTab, $(".build-info"), {});
 
 // Notify the parent frame that the host has been loaded
 VSS.notifyLoadSucceeded();
-
-	
